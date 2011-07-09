6c7cec9e08e6e6a34ec8d62b9c983dd3dfe6771a
Michael Madison <michaelm@simutronics.com>
2011-07-09 15:42:58 -0500

    Added sinatra editor

--- rlgl/ui.rb
+++ rlgl/ui.rb
@@ -10,16 +10,21 @@ class UI
   
   def draw(game_level, window)
     @time_left = (game_level.time_to_next_action - Time.now).ceil
-    arcdist = 360 / game_level.action_interval
-    
-    if @time_left > game_level.action_interval * 0.5
-      color = 0xff00ff00
-    elsif @time_left <= 2.0
-      color = 0xffff0000
+    if @time_left > 0
+      arcdist = 360 / game_level.action_interval
+      
+      if @time_left > game_level.action_interval * 0.5
+        color = 0xff00ff00
+      elsif @time_left <= 2.0
+        color = 0xffff0000
+      else
+        color = 0xffffff00
+      end
     else
-      color = 0xffffff00
+      arcdist = 360 / 8.0
+      color = 0xffff0000
+      @time_left = 7
     end
-    
     
     (0...@time_left).each do |i|
       window.draw_triangle(@timer_circle_x, @timer_circle_y, color, @timer_circle_x+Gosu::offset_x(arcdist*i, @circle_size), @timer_circle_y+Gosu::offset_y(arcdist*i,@circle_size), color, @timer_circle_x+Gosu::offset_x(arcdist*(i+1),@circle_size), @timer_circle_y + Gosu::offset_y(arcdist*(i+1),@circle_size),color, ZOrder::UI)
