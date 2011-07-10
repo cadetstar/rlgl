2cf80220b0575633c5bd2b2b5e777be663b16dba
Michael Madison <michaelm@simutronics.com>
2011-07-09 22:10:48 -0500

    Stubbed in Wifi UI

--- rlgl/ui.rb
+++ rlgl/ui.rb
@@ -1,39 +1,39 @@
 
 class UI
   def initialize(actions, window)
-    @timer_circle_x = 50
-    @timer_circle_y = 50
+    @timer_base = vec2(10,20)
+    @ui_dim = vec2(400,100)
+    @factor = 0.4
     @circle_size = 30
     @actions = actions.collect{|a| %w(jump jump_right jump_left move_right move_left).include?(a[0]) ? a[0] : nil}.compact
     @font = Gosu::Font.new(window, Gosu::default_font_name, 16)
+    @timer_images = []
+    @timer_images << Gosu::Image.new(window, "#{$prefix}media/wifi_4.png", ZOrder::UI)
+    @timer_images << Gosu::Image.new(window, "#{$prefix}media/wifi_3.png", ZOrder::UI)
+    @timer_images << Gosu::Image.new(window, "#{$prefix}media/wifi_2.png", ZOrder::UI)
+    @timer_images << Gosu::Image.new(window, "#{$prefix}media/wifi_1.png", ZOrder::UI)
+    
+    @bgcolor = 0xff000000
   end
   
   def draw(game_level, window)
+    
+    window.draw_quad(0,0,@bgcolor, @ui_dim.x,0,@bgcolor, @ui_dim.x,@ui_dim.y,@bgcolor,0,@ui_dim.y,@bgcolor, ZOrder::UIBackground)
     @time_left = (game_level.time_to_next_action - Time.now).ceil
-    if @time_left > 0
-      arcdist = 360 / game_level.action_interval
-      
-      if @time_left > game_level.action_interval * 0.5
-        color = 0xff00ff00
-      elsif @time_left <= 2.0
-        color = 0xffff0000
-      else
-        color = 0xffffff00
-      end
-      offset = 0
+    if @time_left < 0
+      @time_left = 0
+    end
+    t = @time_left
+    @font.draw(t, 80, 80 - @font.height / 2.0, ZOrder::UIText, 1, 1, 0xffffffff)
+    
+    if t == 0
+      @timer_images[0].draw(@timer_base.x, @timer_base.y, ZOrder::UI, @factor, @factor)
+    elsif t < 4
+      @timer_images[t-1].draw(@timer_base.x, @timer_base.y, ZOrder::UI, @factor, @factor)
     else
-      arcdist = (360 / 8.0)
-      color = 0xffff0000
-      @time_left = 8
-      offset = 22.5
-      t = 'STOP'
-      @font.draw(t, @timer_circle_x-@font.text_width(t)/2.0, @timer_circle_y - @font.height / 2.0, ZOrder::UIText)
+      @timer_images[3].draw(@timer_base.x, @timer_base.y, ZOrder::UI, @factor, @factor)
     end
-    
-    (0...@time_left).each do |i|
-      window.draw_triangle(@timer_circle_x, @timer_circle_y, color, @timer_circle_x+Gosu::offset_x(arcdist*i+offset, @circle_size), @timer_circle_y+Gosu::offset_y(arcdist*i+offset,@circle_size), color, @timer_circle_x+Gosu::offset_x(arcdist*(i+1)+offset,@circle_size), @timer_circle_y + Gosu::offset_y(arcdist*(i+1)+offset,@circle_size),color, ZOrder::UI)
-    end
-    
+
     offset = 125
     space = 50
     pos_y = 50
